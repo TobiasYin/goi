8613bd7f98610a705bd9eb786ce051dc96bfe10a
TobiasYin <yrt7296@gmail.com>
2020-05-05 23:09:37 +0800

    Add muti platform.
    Change-Id: I5a4b22fe4b24feb5875aaae76ebff68fb851a62c

--- tool/build.sh
+++ tool/build.sh
@@ -2,3 +2,5 @@
 
 python3 load_template.py
 go build -o go_web_ui_tool main.go
+GOARCH=amd64 GOOS=windows go build -o go_web_ui_tool.exe main.go
+GOARCH=amd64 GOOS=linux go build -o go_web_ui_tool.out main.go
