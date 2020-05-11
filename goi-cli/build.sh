a86b53f412d0fa8bcf41d3cb3e27a3ef2a700a75
TobiasYin <yrt7296@gmail.com>
2020-05-11 13:17:37 +0800

    Update build name.
    Change-Id: Ia68a7ea75807c31bc4c847c717e174f1a2d02bda

--- goi-cli/build.sh
+++ goi-cli/build.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 python3 load_template.py
-GOARCH=amd64 GOOS=darwin go build -o goi_tool main.go
-GOARCH=amd64 GOOS=windows go build -o goi_tool.exe main.go
-GOARCH=amd64 GOOS=linux go build -o goi_tool.out main.go
-rm inline/init.go
+GOARCH=amd64 GOOS=darwin go build -o goi-cli main.go
+GOARCH=amd64 GOOS=windows go build -o goi-cli.exe main.go
+GOARCH=amd64 GOOS=linux go build -o goi-cli.out main.go
